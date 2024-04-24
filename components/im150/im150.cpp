52fcbfa77d8c432df03b9401c2f55b087a68f756
bernikr <berni98k@gmail.com>
2024-04-24 16:03:53 +0200

    try to generalize the code to other smartmeters (untested) #2

--- components/im150/im150.cpp
+++ components/im150/im150.cpp
@@ -32,43 +32,46 @@ namespace esphome {
         }
 
         void IM150::handle_message(std::vector<uint8_t> msg) {
-            if(msg.size() != 125) {
-                ESP_LOGD(TAG, "wrong msg length: %i, expected 125", msg.size());
-                return;
+            uint8_t datalen = msg.size();
+
+            if(datalen != 125) {
+                ESP_LOGD(TAG, "wrong msg length: %i, expected 125. Functunality is untested with this smartmeter.", msg.size());
             }
+
             if(msg[0] != 0x7e) {
                 ESP_LOGD(TAG, "wrong opening byte: %02x, expected 7e", msg[0]);
                 return;
             }
-            if(msg[124] != 0x7e) {
+            if(msg[datalen-1] != 0x7e) {
                 ESP_LOGD(TAG, "wrong closing byte: %02x, expected 7e", msg[124]);
                 return;
             }
 
             // CRC Check
             int crc = this->CRC16.x25(msg.data() + 1, 121);
-            int expected_crc = msg[123] * 256 + msg[122];
+            int expected_crc = msg[datalen-2] * 256 + msg[datalen-3];
             if(crc != expected_crc) {
                 ESP_LOGD(TAG, "crc mismatch: calculated %04x, expected %04x", crc, expected_crc);
                 return;
             }
             
             // Decrypt
-            uint8_t message[92] = {0};
-            memcpy(message, msg.data() + 30, 92);
+            uint8_t msglen = datalen - 33;
+            uint8_t message[msglen] = {0};
+            memcpy(message, msg.data() + 30, msglen);
             uint8_t nonce[16] = {0};
             memcpy(nonce, msg.data() + 16, 8);
             memcpy(nonce + 8, msg.data() + 26, 4);
             nonce[15] = 0x02;
             this->ctraes128.setKey(this->key, 16);
             this->ctraes128.setIV(nonce, 16);
-            this->ctraes128.decrypt(message, message, 92);
-            ESP_LOGV(TAG, "decrypted data: %s", format_hex_pretty(std::vector<uint8_t>(message, message+92)).c_str());
+            this->ctraes128.decrypt(message, message, msglen);
+            ESP_LOGV(TAG, "decrypted data: %s", format_hex_pretty(std::vector<uint8_t>(message, message+msglen)).c_str());
 
-            uint32_t active_energy_pos_raw    = bytes_to_int(message, 53+5*0, 4);
-            uint32_t active_energy_neg_raw    = bytes_to_int(message, 53+5*1, 4);
-            uint32_t reactive_energy_pos_raw  = bytes_to_int(message, 53+5*2, 4);
-            uint32_t reactive_energy_neg_raw  = bytes_to_int(message, 53+5*3, 4);
+            uint32_t active_energy_pos_raw    = bytes_to_int(message, msglen-4-5*7, 4);
+            uint32_t active_energy_neg_raw    = bytes_to_int(message, msglen-4-5*6, 4);
+            uint32_t reactive_energy_pos_raw  = bytes_to_int(message, msglen-4-5*5, 4);
+            uint32_t reactive_energy_neg_raw  = bytes_to_int(message, msglen-4-5*4, 4);
 
             // use modulo 1000kwh for the energy sensors, because esphome sensors are only 32bit floats
             // values larger than that would suffer from precision errors
@@ -77,10 +80,10 @@ namespace esphome {
             float active_energy_neg    = (active_energy_neg_raw%1000000)/1000.0;
             float reactive_energy_pos  = (reactive_energy_pos_raw%1000000)/1000.0;
             float reactive_energy_neg  = (reactive_energy_neg_raw%1000000)/1000.0;
-            float active_power_pos     = bytes_to_int(message, 53+5*4, 4);
-            float active_power_neg     = bytes_to_int(message, 53+5*5, 4);
-            float reactive_power_pos   = bytes_to_int(message, 53+5*6, 4);
-            float reactive_power_neg   = bytes_to_int(message, 53+5*7, 4);
+            float active_power_pos     = bytes_to_int(message, msglen-4-5*3, 4);
+            float active_power_neg     = bytes_to_int(message, msglen-4-5*2, 4);
+            float reactive_power_pos   = bytes_to_int(message, msglen-4-5*1, 4);
+            float reactive_power_neg   = bytes_to_int(message, msglen-4-5*0, 4);
 
             if(this->active_energy_pos != nullptr && this->active_energy_pos->state != active_energy_pos)
                 this->active_energy_pos->publish_state(active_energy_pos);
